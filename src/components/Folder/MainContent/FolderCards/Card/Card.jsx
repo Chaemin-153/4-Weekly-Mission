55db12dee88be5e3bfbbe84b1d26e78ec4c898c7
Chaemin-153 <0520032@gmail.com>
2024-03-27 15:26:34 +0900

    :lipstick: Style: star, kebab 디자인 수정

--- src/components/Folder/MainContent/FolderCards/Card/Card.jsx
+++ src/components/Folder/MainContent/FolderCards/Card/Card.jsx
@@ -4,26 +4,52 @@ import starImg from '../../../../../images/star.svg';
 import kebabImg from '../../../../../images/kebab.svg';
 import { updateStatus } from '../../../../../utils/cardUpdate';
 import styles from './Card.module.scss';
+import { Link } from 'react-router-dom';
 
-export default function Card({ imageSource, createdAt, description, folder }) {
+export default function Card({
+  imageSource,
+  createdAt,
+  description,
+  folder,
+  url,
+  id,
+}) {
   const date = new Date(createdAt).toLocaleDateString();
 
   const dataStatus = updateStatus(createdAt);
 
   return (
-    <div className={styles.card}>
-      <img
-        src={imageSource ? imageSource : defaultImg}
-        className={styles.cardImg}
-        alt="card"
-      />
-      {folder && <img src={starImg} alt="star" width={34} height={34} />}
-      <div className={styles.textContainer}>
-        <span>{createdAt ? dataStatus : null}</span>
-        <p>{description}</p>
-        <p className={styles.dateNumber}>{date}</p>
-        {folder && <img src={kebabImg} alt="star" width={21} height={17} />}
+    <Link className={styles.cardLink} to={url} key={id}>
+      <div className={styles.card}>
+        <img
+          src={imageSource ? imageSource : defaultImg}
+          className={styles.cardImg}
+          alt="card"
+        />
+        {folder && (
+          <img
+            className={styles.star}
+            src={starImg}
+            alt="star"
+            width={34}
+            height={34}
+          />
+        )}
+        <div className={styles.textContainer}>
+          <span>{createdAt ? dataStatus : null}</span>
+          <p>{description}</p>
+          <p className={styles.dateNumber}>{date}</p>
+          {folder && (
+            <img
+              className={styles.kebab}
+              src={kebabImg}
+              alt="star"
+              width={21}
+              height={17}
+            />
+          )}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
--- src/components/Folder/MainContent/FolderCards/FolderCards.jsx
+++ src/components/Folder/MainContent/FolderCards/FolderCards.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from './Card/Card';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import styles from './FolderCards.module.scss';
 
 export const FolderCards = ({ folder }) => {
@@ -10,16 +10,14 @@ export const FolderCards = ({ folder }) => {
     <div className={`${styles.cardsContainer} ${styles.cardList}`}>
       {isExistFolder ? (
         folder.map(({ id, image_source, created_at, description, url }) => (
-          <Link to={url} key={id}>
-            <Card
-              key={id}
-              imageSource={image_source}
-              createdAt={created_at}
-              description={description}
-              url={url}
-              folder
-            />
-          </Link>
+          <Card
+            key={id}
+            imageSource={image_source}
+            createdAt={created_at}
+            description={description}
+            url={url}
+            folder
+          />
         ))
       ) : (
         <div className={styles.noLink}>저장된 링크가 없습니다</div>
